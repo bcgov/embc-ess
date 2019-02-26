0a92e75123903d472df144e0e0659b11090d1f61
Alejandro Sanchez <emailforasr@gmail.com>
2019-02-26 15:42:28 -0800

    Self registration confirmation page

--- embc-app/ClientApp/src/app/self-registration/self-registration-four/self-registration-four.component.ts
+++ embc-app/ClientApp/src/app/self-registration/self-registration-four/self-registration-four.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+
+// mocking server-side API for now
+const mockResponse = { registrationNumber: '1234 5678' };
 
 @Component({
   selector: 'app-self-registration-four',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./self-registration-four.component.scss']
 })
 export class SelfRegistrationFourComponent implements OnInit {
+  registrationNumber: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.fetchRegistration()
+      .subscribe(reg => {
+        this.registrationNumber = reg.registrationNumber;
+      });
   }
 
+  // TODO: Get this from backend server
+  fetchRegistration() {
+    return of({ ...mockResponse });
+  }
 }
