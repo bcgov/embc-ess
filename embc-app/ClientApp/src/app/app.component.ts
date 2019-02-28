3857986f324650b076d0ff1bfa3fbe634aa4c47d
Alejandro Sanchez <emailforasr@gmail.com>
2019-02-28 14:23:21 -0800

    Add missing files to fix build

--- embc-app/ClientApp/src/app/app.component.ts
+++ embc-app/ClientApp/src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { detectIE10orLower } from './shared/utils/environmentUtils';
+import { forkJoin } from 'rxjs';
+
 import { User } from './core/models';
+import { detectIE10orLower } from './shared/utils/environmentUtils';
+import { ControlledListService } from './core/services/controlled-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
   title = '';
   isIE = false;
   currentUser: User;
+
+  constructor(private lookups: ControlledListService) { }
 
   ngOnInit() {
     this.isIE = detectIE10orLower();
@@ -30,5 +35,23 @@ export class AppComponent  implements OnInit {
 
   get isReturningUser(): boolean {
     return this.isAuthenticated && this.currentUser.isNewUser === false;
+  }
+
+  initializeApp() {
+    // TODO: Load current user (if authenticated)
+
+    // Get lookups.
+    return this.getLookups();
+  }
+
+  getLookups() {
+    return forkJoin([
+      this.lookups.getAllSupportTypes(),
+      this.lookups.getAllRegions(),
+      this.lookups.getAllRegionalDistricts(),
+      this.lookups.getAllCommunities(),
+      this.lookups.getAllFamilyRelationshipTypes(),
+      // ...add more
+    ]);
   }
 }
