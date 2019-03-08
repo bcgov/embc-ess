e002f800589f6de3a024f9db2f67bee76d6f83a8
Curtis <curtis.laycraft@zoho.com>
2019-03-08 11:23:56 -0800

    make fake evacuee service

--- embc-app/ClientApp/src/app/core/services/evacuee.service.ts
+++ embc-app/ClientApp/src/app/core/services/evacuee.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { RestService } from './rest.service';
+import { Observable, of } from 'rxjs';
+import { Evacuee } from '../models';
+import { CoreModule } from '../core.module';
+
+const FAKEEVACUEES: Evacuee[] = [
+  {
+    id: 'qwertyuiop',
+    personType: 'VOLN',
+    firstName: 'John',
+    lastName: 'Doe',
+    nickname: 'Johnny',
+    initials: 'JD',
+    gender: 'yes',
+    dob: null
+  },
+  {
+    id: 'qwertyuiop',
+    personType: 'VOLN',
+    firstName: 'Billy',
+    lastName: 'Donner',
+    nickname: 'BIDO',
+    initials: 'BD',
+    gender: 'yes',
+    dob: null
+  },
+] 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: CoreModule
 })
-export class EvacueeService {
-
-  constructor() { }
+export class EvacueeService extends RestService {
+  getAllEvacuees(): Observable<Evacuee[]> {
+    // simple return fakes
+    return of(FAKEEVACUEES);
+  }
 }
