17bde6059fd7a6471605bc2acb0dff68226bb6e8
Curtis <curtis.laycraft@zoho.com>
2019-03-08 15:38:41 -0800

    Table switch for records

--- embc-app/ClientApp/src/app/core/services/registration.service.ts
+++ embc-app/ClientApp/src/app/core/services/registration.service.ts
@@ -20,6 +20,77 @@ const FAKEREGISTRATIONS: Registration[] = [
     insuranceCode: 'MANU120398',
     medicationNeeds: false,
     selfRegisteredDate: null,
+    registrationCompletionDate: new Date(),
+    registrationFamilyMemberCount: 1,
+    
+    
+    hasThreeDayMedicationSupply: true,
+    hasInquiryReferral: false,
+    hasHealthServicesReferral: false,
+    hasFirstAidReferral: false,
+    hasChildCareReferral: false,
+    hasPersonalServicesReferral: false,
+    hasPetCareReferral: false,
+    hasPets: false,
+    
+    requiresAccommodation: false,
+    requiresClothing: false,
+    requiresFood: false,
+    requiresIncidentals: false,
+    requiresSupport: true,
+    requiresTransportation: true,
+
+    headOfHousehold: 
+      {
+        id: 'qwertyuiop',
+        firstName: 'John',
+        lastName: 'Doe',
+        nickname: 'Johnny',
+        initials: 'JD',
+        gender: 'yes',
+        dob: null,
+        phoneNumber: '',
+        phoneNumberAlt: '',
+        personType: 'HOH',
+        email: 'person@address.org',
+        primaryResidence: null,
+        mailingAddress: null
+      },
+    familyMembers: [],
+    incidentTask: {
+      id: 'aslkdfjh',
+      taskNumber: '10293847',
+      details: 'This is an incident task.',
+      region: null,
+      regionalDistrict: null,
+      community: {
+        id: 'zxoicuvz',
+        name: 'Grand Forks',
+        regionalDistrict: null
+      }
+    },
+    hostCommunity: {
+      id: 'aslkdfjs',
+      name: 'Niagra',
+      regionalDistrict: null
+    },
+    completedBy: null
+  },
+  {
+    id: '123',
+    restrictedAccess: false,
+    essFileNumber:  1289734,
+
+    dietaryNeeds: false,
+    dietaryNeedsDetails: 'gluten intolerance',
+    disasterAffectDetails: 'Freeform text',
+    externalReferralsDetails: 'Freeform text',
+    facility: '',
+    familyRecoveryPlan: '',
+    followUpDetails: '',
+    insuranceCode: 'MANU120398',
+    medicationNeeds: false,
+    selfRegisteredDate: null,
     registrationCompletionDate: null,
     registrationFamilyMemberCount: 1,
     
