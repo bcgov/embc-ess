3e74b97c072c388867d5cb81d9259bc6be469e4f
Curtis <curtis.laycraft@zoho.com>
2019-03-07 16:14:26 -0800

    add dummy router links for volunteer dashboard

--- embc-app/ClientApp/src/app/volunteer-login/volunteer-login.component.ts
+++ embc-app/ClientApp/src/app/volunteer-login/volunteer-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-volunteer-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VolunteerLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  continue(){
+    //this is a simple passthrough for redirecting from the dummy page.
+    alert("Routing home.");
+    this.router.navigateByUrl('/');
 
+  }
 }
