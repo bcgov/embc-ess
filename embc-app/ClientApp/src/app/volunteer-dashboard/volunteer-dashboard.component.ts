22a1073031ab2b63b99d2355b11e0a50b5d93281
Curtis <curtis.laycraft@zoho.com>
2019-03-07 16:16:38 -0800

    create volunteer dashboard routing

---
+++ embc-app/ClientApp/src/app/volunteer-dashboard/volunteer-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-dashboard',
+  templateUrl: './volunteer-dashboard.component.html',
+  styleUrls: ['./volunteer-dashboard.component.scss']
+})
+export class VolunteerDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
