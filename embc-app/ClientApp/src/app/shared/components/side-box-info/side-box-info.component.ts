dbe56f22d14d8e5a71fcc3c76aaa53f97587e756
Curtis <curtis.laycraft@quartech.com>
2019-03-11 15:45:36 -0700

    side box info component

--- embc-app/ClientApp/src/app/shared/components/side-box-info/side-box-info.component.ts
+++ embc-app/ClientApp/src/app/shared/components/side-box-info/side-box-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-box-info',
@@ -6,12 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./side-box-info.component.scss']
 })
 export class SideBoxInfoComponent implements OnInit {
-  @Input() title = 'Sample Title';
-  @Input() icon = 'description';
+  @Input() title = 'TITLE';
+  @Input() content = 'Sample content';
+  @Input() button = 'Ok';
+  @Input() route = '/'; //home by default
 
-  constructor() { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  continue(){
+    //send the user along to the next page submitted
+    this.router.navigateByUrl(this.route);
+  }
 }
