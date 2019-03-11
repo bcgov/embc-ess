9ecec106f5e25910fc4148212493c71905742358
Curtis <curtis.laycraft@quartech.com>
2019-03-11 13:30:16 -0700

    data massage to stubs

--- embc-app/ClientApp/src/app/volunteer-dashboard/volunteer-dashboard.component.ts
+++ embc-app/ClientApp/src/app/volunteer-dashboard/volunteer-dashboard.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Registration, Evacuee } from '../core/models';
+import { Registration } from '../core/models';
 import { RegistrationService } from '../core/services/registration.service';
+
+interface Stub{
+  id: string; // the guid to link them to their file
+  restrictedAccess: boolean; // should this file be shown or not?
+  essFileNumber: number; // what is the ESS file number
+  firstName: string;
+  lastName: string;
+  incidentTaskTaskNumber: string;
+  requiresIncidentals: boolean; // do they need vouchers
+  personType: string; // HOH || FMBR || VOLN
+  evacuatedFrom: string; // community name
+  evacuatedTo: string; // community name
+}
 @Component({
   selector: 'app-volunteer-dashboard',
   templateUrl: './volunteer-dashboard.component.html',
@@ -8,7 +21,7 @@ import { RegistrationService } from '../core/services/registration.service';
 })
 export class VolunteerDashboardComponent implements OnInit {
 
-  registrations;
+  registrations: Stub[];
 
   constructor(
     private registrationService: RegistrationService
@@ -17,10 +30,47 @@ export class VolunteerDashboardComponent implements OnInit {
   ngOnInit() {
     // go get the data
     this.registrationService.getRegistries()
-      .subscribe(r => {
+      .subscribe((registrations: Registration[]) => {
         // save the registrations into the local data blob
-        this.registrations = r;
-      });
+        this.registrations = this.unreduceRegistrationToStubs(registrations);
+    });
   }
+  unreduceRegistrationToStubs(registrations: Registration[]): Stub[]{
+    const stubCollector: Stub[] = [];
+    // loop through registrations and get each family member
+    for (const registration of registrations){
+      // push the head of household as a stub
+      let hoh: Stub = {
+        id: registration.id, // the guid to link them to their file
+        restrictedAccess: registration.restrictedAccess, // should this file be shown or not?
+        essFileNumber: registration.essFileNumber, // what is the ESS file number
+        firstName: registration.headOfHousehold.firstName,
+        lastName: registration.headOfHousehold.lastName,
+        incidentTaskTaskNumber: registration.incidentTask.taskNumber,
+        requiresIncidentals: registration.requiresIncidentals, // do they need vouchers
+        personType: 'HOH', // HOH || FMBR || VOLN
+        evacuatedFrom: registration.incidentTask.community.name, // community name
+        evacuatedTo: registration.hostCommunity.name, // community name
+      };
+      stubCollector.push(hoh);
 
+      // push the family members of the HOH as stubs
+      for (const familyMember of registration.familyMembers){
+        let fmbr = {
+          id: registration.id, // the guid to link them to their file
+          restrictedAccess: registration.restrictedAccess, // should this file be shown or not?
+          essFileNumber:  registration.essFileNumber, // what is the ESS file number
+          firstName: familyMember.firstName,
+          lastName: familyMember.lastName,
+          incidentTaskTaskNumber: registration.incidentTask.taskNumber,
+          requiresIncidentals: registration.requiresIncidentals, // do they need vouchers
+          personType: familyMember.personType, // HOH || FMBR || VOLN
+          evacuatedFrom: registration.incidentTask.community.name, // community name
+          evacuatedTo: registration.hostCommunity.name, // community name
+        };
+        stubCollector.push(fmbr);
+      }
+    }
+    return stubCollector;
+  }
 }
