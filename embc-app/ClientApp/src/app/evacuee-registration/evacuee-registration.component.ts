116f148e10999d6457b370570175ea9e323a29e4
Curtis <curtis.laycraft@zoho.com>
2019-03-12 15:52:10 -0700

    form builder basic

--- embc-app/ClientApp/src/app/evacuee-registration/evacuee-registration.component.ts
+++ embc-app/ClientApp/src/app/evacuee-registration/evacuee-registration.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Store } from '@ngrx/store';
-import { AppState } from '../store';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Registration } from '../core/models';
 
 @Component({
@@ -10,17 +8,59 @@ import { Registration } from '../core/models';
   styleUrls: ['./evacuee-registration.component.scss']
 })
 export class EvacueeRegistrationComponent implements OnInit {
+  
+  // TODO: Delete this demo version of reactive forms.
   name = new FormControl('');
 
-
   // The model for the form data collected
+  form: FormGroup;
+
   // registration: Registration | null;
 
   constructor(
+    private formBuilder: FormBuilder,
     // private store: Store<AppState>, // ngrx app state
-  ) { }
+  ) {
+    // build the form with formbuilder
+    this.initForm();
+  }
 
   ngOnInit() {
   }
 
+  initForm(): void {
+    this.form = this.formBuilder.group({
+      restrictedAccess: null,
+      headOfHousehold: this.formBuilder.group({
+        firstName: '',
+        lastName: '',
+        nickname: '',
+        initials: '',
+        gender: null,
+        dob: null,
+      }),
+      registeringFamilyMembers: null,
+      familyMembers: this.formBuilder.array([]),
+      phoneNumber: '',
+      phoneNumberAlt: '',
+      email: '',
+      primaryResidenceInBC: null,
+      primaryResidence: this.formBuilder.group({
+        addressLine1: '',
+        communityOrCity: '',
+        provinceOrState: '',
+        postalCodeOrZip: '',
+        country: '',
+      }),
+      hasMailingAddress: null,
+      mailingAddressInBC: null,
+      mailingAddress: this.formBuilder.group({
+        addressLine1: '',
+        communityOrCity: '',
+        provinceOrState: '',
+        postalCodeOrZip: '',
+        country: '',
+      }),
+    });
+  }
 }
