f47a60f22746523301457d5fcc09c3a3920018a3
Yossi Tamari <yossi.tamari@quartech.com>
2019-03-20 15:06:15 -0700

    Compiled project

--- embc-app/DataInterfaces/IDataInterface.cs
+++ embc-app/DataInterfaces/IDataInterface.cs
@@ -10,21 +10,20 @@ namespace Gov.Jag.Embc.Public.DataInterfaces
     {
         Organization GetOrganizationByBceidGuid(string bceidGuid);
 
-        Person GetPersonByBceidGuid(string bceidGuid);
-
-        Volunteer GetVolunteerByName(string firstName, string lastName);
-
-        Person CreatePerson(Person person);
-
         // Registrations
         Task<IQueryable<Registration>> GetRegistrations(SearchQueryParameters queryParameters);
+
         Task<Registration> GetRegistration(string id);
+
         Task<Registration> CreateRegistration(Registration registration);
 
         // Incident Tasks
         Task<List<IncidentTask>> GetIncidentTasks();
+
         Task<IncidentTask> GetIncidentTask(string id);
+
         Task<IncidentTask> CreateIncidentTask(IncidentTask task);
+
         Task<IncidentTask> UpdateIncidentTask(IncidentTask task);
 
         List<Community> GetCommunities();
@@ -38,10 +37,21 @@ namespace Gov.Jag.Embc.Public.DataInterfaces
         List<FamilyRelationshipType> GetFamilyRelationshipTypes();
 
         Organization GetOrganizationByLegalName(string name);
+
         Organization GetOrganizationByExternalId(string externalId);
 
-        Volunteer GetVolunteerByExternalId(string externalId);
-        Volunteer GetVolunteerById(string Id);
+        Task<Person> GetPersonByBceidGuidAsync(string bceidGuid);
 
+        Task<Volunteer> GetVolunteerByNameAsync(string firstName, string lastName);
+
+        Task<Person> CreatePersonAsync(Person person);
+
+        Task<bool> DeactivatePersonAsync(string id);
+
+        Task<Volunteer> GetVolunteerByExternalIdAsync(string externalId);
+
+        Task<Volunteer> GetVolunteerByIdAsync(string Id);
+
+        Task<IEnumerable<Volunteer>> GetAllVolunteersAsync();
     }
 }
