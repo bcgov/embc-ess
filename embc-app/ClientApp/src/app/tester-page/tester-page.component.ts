d34bf2ce13bb747daffa6331f1ad2d0ba59518e8
Curtis <curtis.laycraft@zoho.com>
2019-03-20 18:20:42 -0700

    uncomment complete registration

--- embc-app/ClientApp/src/app/tester-page/tester-page.component.ts
+++ embc-app/ClientApp/src/app/tester-page/tester-page.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { GeographicsService } from '../core/services/geographics.service';
+import { IncidentTaskService } from '../core/services/incident-task.service';
+import { RegistrationService } from '../core/services/registration.service';
+import { UserDataService } from '../core/services/user-data.service';
+import { AppState } from '../store';
 
 @Component({
   selector: 'app-tester-page',
@@ -7,9 +14,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TesterPageComponent implements OnInit {
 
-  constructor() { }
+  // state needed by this FORM
+  countries$ = this.store.select(s => s.lookups.countries.countries);
+  regionalDistrics$ = this.store.select(s => s.lookups.regionalDistricts);
+  regions$ = this.store.select(s => s.lookups.regions);
+  relationshipTypes$ = this.store.select(s => s.lookups.relationshipTypes.relationshipTypes);
+  incidentTask$ = this.store.select(s => s.incidentTasks.incidentTasks);
+  // communities$
+
+  constructor(
+    private store: Store<AppState>,
+    private geographicsService: GeographicsService,
+    private incidentTaskService: IncidentTaskService,
+    private registrationService: RegistrationService,
+    private userDataService: UserDataService,
+  ) { }
 
   ngOnInit() {
+
   }
 
 }
