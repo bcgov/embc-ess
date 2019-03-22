60043bd12a608205eab5be2679ac9e1358f996a4
Curtis <curtis.laycraft@zoho.com>
2019-03-22 15:04:31 -0700

    ess user editor page

---
+++ embc-app/ClientApp/src/app/ess-editor/ess-editor.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from './../shared/shared.module';
+import { EssEditorComponent } from './ess-editor.component';
+import { EssEditorOneComponent } from './ess-editor-one/ess-editor-one.component';
+import { EssEditorConfirmationComponent } from './ess-editor-confirmation/ess-editor-confirmation.component';
+
+
+@NgModule({
+    declarations: [
+        EssEditorComponent,
+        EssEditorOneComponent,
+        EssEditorConfirmationComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        SharedModule,
+    ],
+    exports: [
+        EssEditorComponent
+    ]
+})
+export class EvacueeRegistrationModule { }
