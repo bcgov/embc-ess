d2b3566047a0f965bf5774b7658fe3a63862d9c9
Curtis <curtis.laycraft@zoho.com>
2019-03-25 10:48:04 -0700

    load from route

--- embc-app/ClientApp/src/app/evacuee-summary/evacuee-summary.component.ts
+++ embc-app/ClientApp/src/app/evacuee-summary/evacuee-summary.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrationService } from '../core/services/registration.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Registration } from '../core/models';
 
 @Component({
   selector: 'app-evacuee-summary',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvacueeSummaryComponent implements OnInit {
 
-  constructor() { }
+  // collect ess file number from activated route
+  registration: Registration;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private registrationService: RegistrationService,
+  ) { }
 
   ngOnInit() {
+    // collect ess file from activated route
+    this.loadFromRoute();
   }
-
+  loadFromRoute() {
+    // try to collect the ess file number and load it
+    // if there are route params we should grab them
+    if (this.route.snapshot.params.essFileNumber) {
+      // TODO: go get the evacuee from db eventually
+      this.registrationService.getRegistrationByEssFileNumber(this.route.snapshot.params.essFileNumber)
+        .subscribe(r => {
+          // Save the registration into the
+          this.registration = r;
+        });
+    } else {
+      // there is nothing to show? Route back somewhere else. (Volunteer dashboard?)
+    }
+  }
 }
