3d088501559c298ab1e23900ce30c60877477db9
Curtis <curtis.laycraft@zoho.com>
2019-03-25 10:28:41 -0700

    create evacuee summary component

---
+++ embc-app/ClientApp/src/app/evacuee-summary/evacuee-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evacuee-summary',
+  templateUrl: './evacuee-summary.component.html',
+  styleUrls: ['./evacuee-summary.component.scss']
+})
+export class EvacueeSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
