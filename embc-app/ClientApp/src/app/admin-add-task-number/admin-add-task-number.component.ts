0d7979bfbc0d1b6d63ee6993b137670b894c17e2
Curtis <curtis.laycraft@zoho.com>
2019-03-26 18:12:07 -0700

    collect user input

--- embc-app/ClientApp/src/app/admin-add-task-number/admin-add-task-number.component.ts
+++ embc-app/ClientApp/src/app/admin-add-task-number/admin-add-task-number.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from '../store';
+import { Store } from '@ngrx/store';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-add-task-number',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-add-task-number.component.scss']
 })
 export class AdminAddTaskNumberComponent implements OnInit {
+  communities$ = this.store.select(s => s.lookups.communities.communities);
 
-  constructor() { }
+  // three fields to collect
+  taskNumber: FormControl;
+  community: FormControl;
+  details: FormControl;
+
+  constructor(
+    private store: Store<AppState>, // ngrx app state
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
-
+  initForm() {
+    // initialize form
+    this.taskNumber = new FormControl(null);
+    this.community = new FormControl(null);
+    this.details = new FormControl(null);
+  }
+  submit() {
+    alert(this.taskNumber);
+  }
 }
