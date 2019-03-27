d1509477fef1d833e222f63ddee1fab3b17ba577
Curtis <curtis.laycraft@zoho.com>
2019-03-27 10:01:38 -0700

    task number body

--- embc-app/ClientApp/src/app/admin-add-task-number/admin-add-task-number-confirmation/admin-add-task-number-confirmation.component.ts
+++ embc-app/ClientApp/src/app/admin-add-task-number/admin-add-task-number-confirmation/admin-add-task-number-confirmation.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { IncidentTask } from 'src/app/core/models';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
-import { VolunteerService } from 'src/app/core/services/volunteer.service';
 import { takeWhile } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-add-task-number-confirmation',
@@ -13,16 +13,24 @@ import { takeWhile } from 'rxjs/operators';
 export class AdminAddTaskNumberConfirmationComponent implements OnInit {
   componentActive = true;
   currentIncidentTask$ = this.store.select(s => s.incidentTasks.currentIncidentTask);
+
   incidentTask: IncidentTask;
 
   constructor(
     private store: Store<AppState>,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.currentIncidentTask$.pipe(takeWhile(() => this.componentActive))
       .subscribe(i => {
-        this.incidentTask = i;
+        // if there are any mandatory fields that are null route back to the main component
+        if (!i.taskNumber || !i.community || !i.details) {
+          this.router.navigate(['add-task-number']);
+        } else {
+          // we have somethings to show so we show it.
+          this.incidentTask = i;
+        }
       });
   }
 
