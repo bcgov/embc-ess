1634187dcf9a615bfb0c26f1f73d8f0aa665a8ca
Curtis <curtis.laycraft@zoho.com>
2019-03-27 20:54:08 -0700

    make admin dashboard show evacuees

--- embc-app/ClientApp/src/app/admin-dashboard/admin-dashboard.component.ts
+++ embc-app/ClientApp/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { IncidentTaskService } from '../core/services/incident-task.service';
-import { MetaIncidentTask } from '../core/models/meta-incident-task';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MetaRegistration } from '../core/models/meta-registration';
+import { EvacueeSearchResults, SearchQueryParameters } from '../shared/components/search';
+import { RegistrationService } from '../core/services/registration.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -8,14 +12,52 @@ import { MetaIncidentTask } from '../core/models/meta-incident-task';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-  incidentTasks: MetaIncidentTask;
+  // server response
+  resultsAndPagination$: Observable<MetaRegistration>;
+
+  // search related
+  isLoadingResults = false;
+  searchState = { offset: 0, limit: 100, sort: '', query: '' };
+  searchResults$: Observable<EvacueeSearchResults>;
 
   constructor(
-    private incidentTaskService: IncidentTaskService
+    private registrationService: RegistrationService,
+    private router: Router,
+
   ) { }
 
   ngOnInit() {
-    this.incidentTaskService.getIncidentTasks().subscribe(i => this.incidentTasks = i);
+    this.doSearch();
   }
 
+  doSearch(query: string = '') {
+    // update form state
+    this.searchState.query = query || '';
+    this.isLoadingResults = true;
+
+    // go get a fresh list of registrations from the service
+    const queryParams: SearchQueryParameters = {
+      offset: this.searchState.offset,
+      limit: this.searchState.limit,
+      sort: this.searchState.sort || '',
+      q: this.searchState.query
+    };
+    this.resultsAndPagination$ = this.registrationService.getRegistrations(queryParams);
+
+    // process server response into something we can display in the UI
+    this.searchResults$ = this.resultsAndPagination$.pipe(
+      map(x => {
+        // Flip flag to show that loading has finished.
+        this.isLoadingResults = false;
+        return { results: x.data, query } as EvacueeSearchResults;
+      })
+    );
+
+    // TODO: store the pagination metadata + links somewhere
+  }
+
+  routeTo(essFileNumber: string) {
+    // TODO: this seems like bad practive but fix when we have time
+    this.router.navigate(['register-evacuee/fill/' + essFileNumber]);
+  }
 }
