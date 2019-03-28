095bf8f992983fd2797505515a7129d956bfd8af
Curtis <curtis.laycraft@zoho.com>
2019-03-28 15:35:05 -0700

    create canactivate

---
+++ embc-app/ClientApp/src/app/core/services/auth-guard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(
+    public router: Router,
+  ) { }
+
+  canActivate(): boolean {
+    // If the user state exists and it matches the link we return true
+    // else we return false
+    return true;
+    /**
+    Todo: list of routes and who should be able to access them
+    */
+
+  }
+}
