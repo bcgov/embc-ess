bb490222dfa267f3d2f3766d6b70e28279ed075e
Curtis <curtis.laycraft@zoho.com>
2019-03-29 14:20:18 -0700

    flow data into add task number

--- embc-app/ClientApp/src/app/admin-add-task-number/admin-add-task-number-one/admin-add-task-number-one.component.ts
+++ embc-app/ClientApp/src/app/admin-add-task-number/admin-add-task-number-one/admin-add-task-number-one.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { IncidentTask } from 'src/app/core/models';
 import { UpdateIncidentTask } from 'src/app/store/incident-tasks/incident-tasks.actions';
 import { takeWhile } from 'rxjs/operators';
+import { IncidentTaskService } from 'src/app/core/services/incident-task.service';
 
 @Component({
   selector: 'app-admin-add-task-number-one',
@@ -13,6 +14,7 @@ import { takeWhile } from 'rxjs/operators';
   styleUrls: ['./admin-add-task-number-one.component.scss']
 })
 export class AdminAddTaskNumberOneComponent implements OnInit {
+  editMode = false;
 
   communities$ = this.store.select(s => s.lookups.communities.communities);
   // whatever is in the application state
@@ -23,39 +25,59 @@ export class AdminAddTaskNumberOneComponent implements OnInit {
   taskNumber: FormControl;
   community: FormControl;
   details: FormControl;
-  incidentTask: IncidentTask;
+  incidentTask: IncidentTask = {
+    id: '',
+    taskNumber: '',
+    details: '',
+    community: null,
+  };
 
   constructor(
+    private route: ActivatedRoute,
     private router: Router,
     private store: Store<AppState>,
+    private incidentTaskService: IncidentTaskService,
   ) { }
 
   ngOnInit() {
     // initialize form for collection
     this.initForm();
+    if (this.route.snapshot.params.id) {
+      // there may be a user to edit because the route looks right
+      this.incidentTaskService.getIncidentTask(this.route.snapshot.params.id).subscribe((i: IncidentTask) => {
+        // save the volunteer for filling in information later.
+        this.taskNumber.setValue(i.taskNumber);
+        this.community.setValue(i.community);
+        this.details.setValue(i.details);
+
+        this.incidentTask = i;
+        this.editMode = true;
+      });
+    } else {
+      // this is a fresh form and will be a simple add user
+      this.editMode = false;
+    }
   }
   initForm() {
     // get the current incident task
-    this.currentIncidentTask$.pipe(takeWhile(() => this.componentActive))
-      .subscribe(i => {
-        this.incidentTask = i;
-        // initialize form
-        this.taskNumber = new FormControl(i.taskNumber || null);
-        this.community = new FormControl(i.community || null);
-        this.details = new FormControl(i.details || null);
-      });
+    this.taskNumber = new FormControl('');
+    this.details = new FormControl('');
+    this.community = new FormControl('');
+    // this.currentIncidentTask$.pipe(takeWhile(() => this.componentActive))
+    //   .subscribe(i => {
+    //     this.incidentTask = i;
+    //     // initialize form
+    //     this.taskNumber = new FormControl(i.taskNumber || null);
+    //     this.community = new FormControl(i.community || null);
+    //     this.details = new FormControl(i.details || null);
+    //   });
   }
   next(): void {
+    this.onSave();
     // only go next if all fields are non null
     if (this.taskNumber.value && this.community.value && this.details.value) {
-      const incidentTask: IncidentTask = {
-        id: this.incidentTask.id || null, // keep the id for updates
-        taskNumber: this.taskNumber.value,
-        community: this.community.value,
-        details: this.details.value,
-      };
       // when routing to the next page we save first into the application state.
-      this.onSave(incidentTask);
+      this.onSave();
       // information saved in state. Navigate to confirm page
       this.router.navigate(['task-number-edit/confirmation']);
     } else {
@@ -63,7 +85,12 @@ export class AdminAddTaskNumberOneComponent implements OnInit {
     }
   }
 
-  onSave(incidentTask: IncidentTask): void {
+  onSave(): void {
+    const incidentTask: IncidentTask = this.incidentTask;
+    incidentTask.id = this.incidentTask.id || null; // keep the id for updates
+    incidentTask.taskNumber = this.taskNumber.value;
+    incidentTask.community = this.community.value;
+    incidentTask.details = this.details.value;
     // stuff the data into an incidentTask object
     this.store.dispatch(new UpdateIncidentTask({ incidentTask }));
   }
