10ad5e390b78e893dee0a9e2563fb209ec9663c6
Alejandro Sanchez <emailforasr@gmail.com>
2019-03-30 10:57:23 -0700

    Add initial protected/guarded routing

--- embc-app/ClientApp/src/app/app-routing.module.ts
+++ embc-app/ClientApp/src/app/app-routing.module.ts
@@ -28,6 +28,8 @@ import { AdminTaskNumbersComponent, AdminEvacueesComponent, AdminOrganizationsCo
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { LoggedInGuard } from './core/guards/logged-in.guard';
 import { RoleGuard } from './core/guards/role.guard';
+import { VOLUNTEER } from './constants';
+import { VolunteerLayoutComponent } from './volunteers/containers/volunteer-layout/volunteer-layout.component';
 
 /**
   /
@@ -102,6 +104,26 @@ const routes: Routes = [
       },
     ]
   },
+
+  // TODO: New naming starts HERE
+
+  {
+    path: 'volunteer',
+    component: VolunteerLayoutComponent,
+    canActivate: [LoggedInGuard],
+    children: [
+      {
+        path: 'evacuees',
+        component: VolunteerDashboardComponent,
+        data: { expectedRole: VOLUNTEER },
+      },
+    ],
+  },
+
+
+  // TODO: Remove/review routes BELOW HERE
+
+
   {
     path: 'useful-info',
     component: VolunteerUsefulInformationComponent,
@@ -109,12 +131,6 @@ const routes: Routes = [
   {
     path: 'volunteer/useful-info',
     component: VolunteerUsefulInformationComponent,
-  },
-  {
-    path: 'volunteer/evacuees',
-    component: VolunteerDashboardComponent,
-    canActivate: [LoggedInGuard],
-    // data: { expectedRole: 'volunteer' }
   },
   {
     path: 'volunteer-dashboard',
