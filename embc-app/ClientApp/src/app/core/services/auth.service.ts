f6c0daa87b191c4b98ff008234bc225dd751ffad
Curtis <curtis.laycraft@zoho.com>
2019-04-01 12:00:37 -0700

    add behavior subject

--- embc-app/ClientApp/src/app/core/services/auth.service.ts
+++ embc-app/ClientApp/src/app/core/services/auth.service.ts
@@ -5,14 +5,15 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { RestService } from './rest.service';
 import { User } from '../models';
 import { doesNotThrow } from 'assert';
+import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService extends RestService {
 
-  // // this is the current user object
-  // currentUser$ = new BehaviorSubject<User>(null);
+  // this is the current user object
+  role = new BehaviorSubject<User>(null);
 
   // // whether the user is currently logged in.
   // isLoggedIn$ = new BehaviorSubject<boolean>(false);
@@ -44,16 +45,26 @@ export class AuthService extends RestService {
     // check local cache first
     if (this.currentUser) {
       return of(this.currentUser);
+
     }
 
     return this.http.get<User>('api/users/current', { headers: this.headers })
-      .pipe(catchError(() => of<User>(null))); // ignore errors; i.e. 401 error means "no current user available/logged in"
+      .pipe(
+        tap(user => {
+          // if the response worked we should move the contents to a behaviorSubject
+          if (user instanceof HttpResponse) {
+            this.role.next(user);
+          }
+        }),
+        catchError(() => of<User>(null))); // ignore errors; i.e. 401 error means "no current user available/logged in"
   }
 
   // this sets a current user to a value to test the current user observable all subscribers should update.
   setCurrentUser(user: User): void {
     // this.currentUser$.next(user);
     this.currentUser = user;
+    // update the behavioursubject
+    this.role.next(user);
   }
 
   // setLoggedIn(value: boolean): void {
