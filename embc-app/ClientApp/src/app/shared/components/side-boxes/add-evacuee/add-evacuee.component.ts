a69d263da67c2abe34cbd58c981e67d095fc814a
Alejandro Sanchez <emailforasr@gmail.com>
2019-04-10 11:35:03 -0700

    Fix for EMBCESS-481
    As a provincial admin, I want to be able to click on the side bar box to add an Evacuee

--- embc-app/ClientApp/src/app/shared/components/side-boxes/add-evacuee/add-evacuee.component.ts
+++ embc-app/ClientApp/src/app/shared/components/side-boxes/add-evacuee/add-evacuee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-evacuee',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-evacuee.component.scss']
 })
 export class AddEvacueeComponent implements OnInit {
+
+  @Input() route = '../register-evacuee'; // TODO: don't pass route in when routing is flattened corrected for all components that use THIS component
+  @Input() absoluteRouting = false;
 
   constructor() { }
 
