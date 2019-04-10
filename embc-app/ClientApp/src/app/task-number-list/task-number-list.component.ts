8bf87719ed2a794603c4a5597f26883f0162cbee
Curtis <curtis.laycraft@zoho.com>
2019-04-10 11:58:25 -0700

    fix breaking changes

--- embc-app/ClientApp/src/app/task-number-list/task-number-list.component.ts
+++ embc-app/ClientApp/src/app/task-number-list/task-number-list.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { Store } from '@ngrx/store';
-
-import { AppState } from '../store';
-import { ListResult, IncidentTask, PaginationSummary, Community } from '../core/models';
+import { ListResult, IncidentTask } from '../core/models';
 import { IncidentTaskService } from '../core/services/incident-task.service';
-import { SearchQueryParameters } from '../shared/components/search';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-number-list',
@@ -14,8 +9,6 @@ import { SearchQueryParameters } from '../shared/components/search';
   styleUrls: ['./task-number-list.component.scss']
 })
 export class TaskNumberListComponent implements OnInit {
-
-  communities$ = this.store.select(s => s.lookups.communities.communities);
 
   // simple server response
   metaIncidentTasks: ListResult<IncidentTask>;
@@ -31,39 +24,23 @@ export class TaskNumberListComponent implements OnInit {
   previousQuery: string; // a place to save the last query parameters
   sort: string = ''; // how do we sort the list
   collectionSize: number = 0; // how large is the collection?
-  maxSize = 100; // how many results should the UI show?
+  maxSize = 5; // how many results should the UI show?
   boundaryLinks = true; // do we show the jump to first and last page links?
-  resultsAndPagination: ListResult<IncidentTask>;
-  notFoundMessage = 'Searching ...';
-
-  form: FormGroup;
 
   constructor(
     private incidentTaskService: IncidentTaskService,
     private router: Router,
     private route: ActivatedRoute,
-    private store: Store<AppState>,
-    private fb: FormBuilder,
   ) { }
 
-  // convenience getters
-  get results(): IncidentTask[] {
-    return this.resultsAndPagination ? this.resultsAndPagination.data : null;
-  }
-
-  get pagination(): PaginationSummary {
-    return this.resultsAndPagination ? this.resultsAndPagination.metadata : null;
-  }
-
   ngOnInit() {
-    this.initSearchForm();
-
     // collect all volunteers
     this.getIncidentTasks();
   }
 
-  initSearchForm(): void {
-    this.form = this.fb.group({ searchbox: null });
+  routeTo(id: string) {
+    // TODO: this seems like bad practice but fix when we have time
+    this.router.navigate(['../task-number/' + id], { relativeTo: this.route });
   }
 
   getIncidentTasks(limit?: number, offset?: number, query?: string, sort?: string) {
@@ -71,19 +48,11 @@ export class TaskNumberListComponent implements OnInit {
     this.incidentTaskService.getIncidentTasks().subscribe((v: ListResult<IncidentTask>) => {
       // save the metaVolunteers
       this.metaIncidentTasks = v;
-      // collect all of the meta into variables
-      this.page = v.metadata.page;
-      this.totalPages = v.metadata.totalPages;
-      this.collectionSize = v.metadata.totalCount;
-      this.maxSize = v.metadata.pageSize;
-      // alert(v.metadata.pageSize)
-      //save the last query performed
-      this.previousQuery = query || '';
     });
   }
 
-  filter(community: Community) {
+  search(searchTerm: string) {
     // submit and collect search
-    this.getIncidentTasks({ q: community ? community.id : '' });
+    this.getIncidentTasks(null, null, searchTerm);
   }
 }
