9866cc6ab5d8ef184b1eb97c77f9c1bb4daa6509
Curtis <curtis.laycraft@zoho.com>
2019-04-23 15:12:00 -0700

    collect path for routing in task-number

--- embc-app/ClientApp/src/app/components/sidebox-add-task-number/sidebox-add-task-number.component.ts
+++ embc-app/ClientApp/src/app/components/sidebox-add-task-number/sidebox-add-task-number.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-sidebox-add-task-number',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideboxAddTaskNumberComponent implements OnInit {
 
-  route = '../task-number';
-  constructor() { }
+  route: string;
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.path.subscribe(p => {
+      this.route = `/${p}/task-number`;
+    });
   }
 
 }
--- embc-app/ClientApp/src/app/components/sidebox-add-volunteer/sidebox-add-volunteer.component.ts
+++ embc-app/ClientApp/src/app/components/sidebox-add-volunteer/sidebox-add-volunteer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-sidebox-add-volunteer',
@@ -6,10 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./sidebox-add-volunteer.component.scss']
 })
 export class SideboxAddVolunteerComponent implements OnInit {
-  route = '../volunteer';
+  route: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.authService.path.subscribe(p => {
+      this.route = `/${p}/volunteer`;
+    });
+  }
 
 }
