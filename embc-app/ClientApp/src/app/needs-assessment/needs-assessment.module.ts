48231a75f142d02474edd7f7e263070f17b2d4f7
Curtis <curtis.laycraft@zoho.com>
2019-04-25 16:02:00 -0700

    Html layout skeleton

--- embc-app/ClientApp/src/app/needs-assessment/needs-assessment.module.ts
+++ embc-app/ClientApp/src/app/needs-assessment/needs-assessment.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IncidentalsComponent } from './incidentals/incidentals.component';
+// import { FontAwesomeLinkComponent } from '../shared/components/fa-link/fa-link.component';
 
 @NgModule({
   declarations: [IncidentalsComponent],
   imports: [
-    CommonModule
+    // FontAwesomeLinkComponent,
+    CommonModule,
   ],
   exports: [
     IncidentalsComponent, // export to build in test page
