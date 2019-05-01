0723552cd6307c90ee0eb1dffe2b23f83f0f4a69
Yossi Tamari <yossi.tamari@quartech.com>
2019-05-01 11:09:00 -0700

    EMBCESS-543: added missing fields to registration summary view model

--- embc-app/ViewModels/RegistrationSummary.cs
+++ embc-app/ViewModels/RegistrationSummary.cs
@@ -1,35 +1,27 @@
 using System;
-using System.Collections.Generic;
 
 namespace Gov.Jag.Embc.Public.ViewModels
 {
-
     public class RegistrationSummary
     {
         public string Id { get; set; }
-
-        // important
         public bool? RestrictedAccess { get; set; }
         public bool? DeclarationAndConsent { get; set; }
         public long? EssFileNumber { get; set; }
-
-        // registration record
         public DateTimeOffset? SelfRegisteredDate { get; set; }
         public DateTimeOffset? RegistrationCompletionDate { get; set; }
         public string RegisteringFamilyMembers { get; set; }  // one of ['yes', 'yes-later', 'no']
-        
-        // requirements
         public bool? RequiresAccommodation { get; set; }
         public bool? RequiresClothing { get; set; }
         public bool? RequiresFood { get; set; }
         public bool? RequiresIncidentals { get; set; }
         public bool? RequiresTransportation { get; set; }
         public bool? RequiresSupport { get; set; }
-
-        // related entities
         public HeadOfHousehold HeadOfHousehold { get; set; }
         public IncidentTask IncidentTask { get; set; }
         public Community HostCommunity { get; set; }
-
+        public bool Active { get; set; }
+        public bool HasFollowUpDetails { get; set; }
+        public string Facility { get; set; }
     }
 }
