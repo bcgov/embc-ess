2227beb1c5416ac6549db7b072cb329fb49f2155
Curtis <curtis.laycraft@zoho.com>
2019-05-03 15:02:47 -0700

    more cleanup

--- embc-app/ClientApp/src/app/tester-page/tester-page.component.ts
+++ embc-app/ClientApp/src/app/tester-page/tester-page.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NotificationQueueService } from '../core/services/notification-queue.service';
-import { ReferralDate } from '../core/models/referral-date';
-import { Registration } from 'src/app/core/models';
-import { Referral } from 'src/app/core/models';
+
 
 @Component({
   selector: 'app-tester-page',
@@ -10,38 +7,8 @@ import { Referral } from 'src/app/core/models';
   styleUrls: ['./tester-page.component.scss']
 })
 export class TesterPageComponent implements OnInit {
-  referral: Referral = null;
-  page = 1;
-  dateStub: any;
-
-  // inputDate: ReferralDate = { identifier: 'banana' };
-  inputDate: ReferralDate = {
-    from: new Date(),
-    days: 1
-  };
-
   constructor(
-    private notifications: NotificationQueueService
   ) { }
 
   ngOnInit() { }
-
-  notify(message?: string, type?: string) {
-    this.notifications.addNotification(message, 6000, type);
-  }
-
-  doRemove() {
-    console.log('remove event');
-  }
-
-  doAdd() {
-    console.log('add event');
-  }
-
-  onDate(event: string) {
-    // for checking the object
-    if (event) {
-      this.dateStub = event;
-    }
-  }
 }
