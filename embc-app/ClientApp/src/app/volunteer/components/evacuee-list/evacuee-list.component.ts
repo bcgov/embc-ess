2ea17c266214f85c1016186073531c1e19864b13
Curtis <curtis.laycraft@zoho.com>
2019-05-06 13:04:21 -0700

    emit value on change

--- embc-app/ClientApp/src/app/volunteer/components/evacuee-list/evacuee-list.component.ts
+++ embc-app/ClientApp/src/app/volunteer/components/evacuee-list/evacuee-list.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Evacuee } from 'src/app/core/models';
 
+interface EvacueeSelector {
+  evacuee: Evacuee;
+  selected: boolean;
+}
 @Component({
   selector: 'app-evacuee-list',
   templateUrl: './evacuee-list.component.html',
   styleUrls: ['./evacuee-list.component.scss']
 })
 export class EvacueeListComponent implements OnInit {
+  @Input() evacuees: EvacueeSelector[];
+  @Output() evacueesChange = new EventEmitter<EvacueeSelector[]>();
 
   constructor() { }
-
-  evacuees = [
-    { firstName: 'Anne', lastName: 'Smith', checked: false },
-    { firstName: 'Jack', lastName: 'Smith', checked: false },
-    { firstName: 'Robert', lastName: 'Smith', checked: true },
-    { firstName: 'Joan', lastName: 'Smith', checked: false }
-  ];
 
   ngOnInit() {
   }
 
+  selectAll() {
+    // select all evacuees
+    this.evacuees = this.evacuees.map(evacuee => {
+      evacuee.selected = true;
+      return evacuee;
+    });
+  }
+  emitList() {
+    // output the modified list
+    this.evacueesChange.emit(this.evacuees);
+  }
 }
