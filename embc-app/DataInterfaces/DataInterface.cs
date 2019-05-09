cb4b17bff5930debdd29ea9e1ffd1d499ac5dbb2
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-05-09 06:27:53 -0700

    Fix issues with Volunteer ID missing and Issue with updating existing Registrations

--- embc-app/DataInterfaces/DataInterface.cs
+++ embc-app/DataInterfaces/DataInterface.cs
@@ -1,12 +1,16 @@
+using AutoMapper;
+
 namespace Gov.Jag.Embc.Public.DataInterfaces
 {
     public partial class DataInterface : IDataInterface
     {
         private readonly EmbcDbContext db;
+        private readonly IMapper mapper;
 
-        public DataInterface(EmbcDbContext ctx)
+        public DataInterface(EmbcDbContext ctx, IMapper mapper)
         {
             db = ctx;
+            this.mapper = mapper;
         }
     }
 }
