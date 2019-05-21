87344c271d882511ae07c88f218c6bb945250870
Alejandro Sanchez <emailforasr@gmail.com>
2019-05-21 15:27:54 -0700

    Incidentals referral form changes

--- embc-app/ClientApp/src/app/volunteer/components/incidentals-referral/incidentals-referral.component.ts
+++ embc-app/ClientApp/src/app/volunteer/components/incidentals-referral/incidentals-referral.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
-import { IncidentalsReferral, Supplier } from 'src/app/core/models';
+import { IncidentalsReferral } from 'src/app/core/models';
 import { IncidentalsRatesComponent } from 'src/app/shared/modals/incidentals-rates/incidentals-rates.component';
 import { AbstractReferralComponent } from '../abstract-referral/abstract-referral.component';
 import { CustomValidators } from 'src/app/shared/validation/custom.validators';
+import { ReferralDate } from 'src/app/core/models/referral-date';
 
 const MAXIMUM_PER_PERSON = 50.00;
 
@@ -14,7 +15,7 @@ const MAXIMUM_PER_PERSON = 50.00;
   templateUrl: './incidentals-referral.component.html',
   styleUrls: ['./incidentals-referral.component.scss']
 })
-export class IncidentalsReferralComponent extends AbstractReferralComponent<IncidentalsReferral> implements OnInit, OnDestroy, OnChanges {
+export class IncidentalsReferralComponent extends AbstractReferralComponent<IncidentalsReferral> implements OnInit, OnDestroy {
   private ratesModal: NgbModalRef = null;
 
   get maximumAmount() {
@@ -35,66 +36,29 @@ export class IncidentalsReferralComponent extends AbstractReferralComponent<Inci
   }
 
   ngOnInit() {
-    super.ngOnInit(); // this is IMPORTANT! - failure to call the base class will stop validation from working!
-
-    // TODO: Review these below...
-    // this.handleFormChange();
+    // this is IMPORTANT! - failure to call the base class will stop validation from working!
+    super.ngOnInit();
   }
 
   ngOnDestroy() {
     super.ngOnDestroy();
-
     // close modal if it's open
     if (this.ratesModal) { this.ratesModal.dismiss(); }
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.referral && this.referral) {
-      this.displayReferral(this.referral);
-    }
-    if (changes.readOnly) {
-      // console.log('readOnly =', changes.readOnly.currentValue);
-    }
+  fromModel(referral: IncidentalsReferral) {
+    // populate fields shared by all forms; e.g. selected evacuees, comments, supplier info...
+    super.fromModel(referral);
+    this.form.patchValue({
+      approvedItems: referral.approvedItems,
+      totalAmount: referral.totalAmount,
+    });
   }
 
-  // validate the whole form as we capture data
-  // handleFormChange(): void {
-  //   this.form.valueChanges.subscribe(() => this.saveChanges());
-  // }
-
-  displayReferral(referral: IncidentalsReferral) {
-    if (referral && !this.readOnly) {
-      this.form.reset();
-      this.form.patchValue({
-        evacuees: referral.evacuees,
-        approvedItems: referral.approvedItems,
-        totalAmount: referral.totalAmount,
-        comments: referral.comments,
-        // supplier: referral.supplier,
-      });
-
-      // populate the evacuee list with existing selection
-      (referral.evacuees || []).forEach(x => this.selectEvacuee(x));
-
-      // TODO: Dates FROM and TO
-    }
+  // NB: this is called when date component is initialized and whenever its data changes
+  updateReferralDate(rd: ReferralDate) {
+    this.referral.dates = rd;
   }
-
-  // if all required information is in the form we emit
-  // saveChanges() {
-  //   if (!this.form.valid) {
-  //     return;
-  //   }
-  //   // Copy over all of the original referral properties
-  //   // Then copy over the values from the form
-  //   // This ensures values not on the form, such as the Id, are retained
-  //   const p: IncidentalsReferral = { ...this.referral, ...this.form.value };
-  //   this.propagateChanges(p);
-  // }
-
-  // updateSupplier(value: Supplier) {
-  //   this.referral.supplier = value;
-  // }
 
   viewRates() {
     this.ratesModal = this.modals.open(IncidentalsRatesComponent, { size: 'lg', centered: true });
