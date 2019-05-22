ea002c3f3935443ac75aced75169af0024ab255c
Severin Beauvais <severin.beauvais@gmail.com>
2019-05-22 10:39:38 -0700

    fix Valid Dates not displayed on View Referral page

--- embc-app/ClientApp/src/app/volunteer/pages/referral-view/referral-view.component.ts
+++ embc-app/ClientApp/src/app/volunteer/pages/referral-view/referral-view.component.ts
@@ -59,7 +59,6 @@ export class ReferralViewComponent implements OnInit, OnDestroy {
           this.goHome();
         } else {
           this.referral = x.referral;
-          this.referral.validDates = { from: x.referral.validFrom, to: x.referral.validTo };
         }
       }, err => {
         this.loading = false;
