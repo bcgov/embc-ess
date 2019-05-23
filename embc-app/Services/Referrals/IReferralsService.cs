db15d5015d2a053aa9aa37184d880354e53c9e9b
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-05-23 12:40:21 -0700

    Removing uneeded code

--- embc-app/Services/Referrals/IReferralsService.cs
+++ embc-app/Services/Referrals/IReferralsService.cs
@@ -7,8 +7,6 @@ namespace Gov.Jag.Embc.Public.Services.Referrals
     {
         Task<byte[]> GetReferralPdfsAsync(ReferralsToPrint printReferrals);
 
-        Task<string> GetReferralHtmlPagesAsync(ReferralsToPrint printReferrals);
-
         bool IsValidReferralType(string type, string subType);
     }
 }
