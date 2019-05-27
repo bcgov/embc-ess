13bbd718e094f7a2b851d65cee6419a9441be22a
Curtis <curtis.laycraft@zoho.com>
2019-05-27 09:52:10 -0700

    logging

--- pdf-service/src/api/routes.ts
+++ pdf-service/src/api/routes.ts
@@ -10,11 +10,14 @@ export default function applyRoutes(app: Express) {
   app.post('/pdf', async function (req: Request, res: Response) {
     const { filename = 'unnamed.pdf' } = req.query;
     const html = req.body;
+    console.log('Request to print ' + filename);
     const pdf = await printPDF(html);
+    console.log(`Sending back PDF of length ${pdf.length}`);
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Length', pdf.length);
     res.setHeader('Content-Disposition', `inline;filename=${filename}`);
     res.send(pdf);
+    console.log(`Done`)
   });
 
   /**
