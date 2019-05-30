d65ad21d17eff9e7384ad10429d86e35ee5fd3f3
Yossi Tamari <yossi.tamari@quartech.com>
2019-05-30 16:11:57 -0700

    Revert "Fixed filter org by community or region, removed Id property from region view model as it is not used"
    This reverts commit 6636fe511732c283efc270415f4843448ad276c2.

--- embc-app/ViewModels/Region.cs
+++ embc-app/ViewModels/Region.cs
@@ -1,10 +1,10 @@
 namespace Gov.Jag.Embc.Public.ViewModels
 {
+
     public class Region
     {
-        //public string Id { get; set; }
+        public string Id { get; set; }
         public bool? Active { get; set; }
-
         public string Name { get; set; }
     }
 }
