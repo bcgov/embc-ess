8ec610fc760f2230af527aed3e58d3b57db44802
Curtis <curtis.laycraft@zoho.com>
2019-05-31 16:45:25 -0700

    bunch of style changes

--- embc-app/ClientApp/src/app/components/top-navbar/top-navbar.component.ts
+++ embc-app/ClientApp/src/app/components/top-navbar/top-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { PROVINCIAL_ADMIN, LOCAL_AUTHORITY, VOLUNTEER } from 'src/app/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-navbar',
@@ -20,6 +21,7 @@ export class TopNavbarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
