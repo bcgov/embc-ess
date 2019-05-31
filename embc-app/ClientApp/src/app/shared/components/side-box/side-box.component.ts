f00db56a5fe6a501390d7ca65972eb04ad120079
Curtis <curtis.laycraft@zoho.com>
2019-05-31 15:38:05 -0700

    side box refactor

--- embc-app/ClientApp/src/app/shared/components/side-box/side-box.component.ts
+++ embc-app/ClientApp/src/app/shared/components/side-box/side-box.component.ts
@@ -4,27 +4,29 @@ import { Component, Input } from '@angular/core';
   selector: 'app-side-box',
   styleUrls: ['./side-box.component.scss'],
   template: `
-    <mat-card class="side-box shadow">
-      <mat-card-header>
-        <h2 mat-card-avatar class="round-icon-material">
-          <mat-icon class="icon" aria-label="description icon">{{icon}}</mat-icon>
-        </h2>
-        <mat-card-title>
-          <h2>{{title}}</h2>
-        </mat-card-title>
-      </mat-card-header>
-      <mat-card-content>
-        <ng-content select="section[role=content]"></ng-content>
-      </mat-card-content>
-      <mat-card-actions align="end">
-        <ng-content select="section[role=footer]"></ng-content>
-      </mat-card-actions>
-    </mat-card>
+
+    <div class="card text-white bg-primary">
+      <div class="card-header">
+      <app-font-awesome-icon icon="info-circle" size="lg"></app-font-awesome-icon>
+      <span class="h5 ml-2 text-wrap">Useful Information</span>
+      </div>
+      <div class="card-body">
+        <!--<span class="h2 card-title">{{secondaryTitle}}</span>-->
+        <p class="card-text">
+          Evacuees listed here that do not have a task number are self-registered and have not yet received Emergency Support Services.
+        </p>
+        <p class="card-text">
+          Use the “Action” column to “view” evacuee registrations as well as “finalize” evacuee self-registrations.
+        </p>
+      </div>
+    </div>
   `,
 })
 export class SideBoxComponent {
 
-  @Input() title = 'Sample Title';
+  @Input() title = '';
+  @Input() secondaryTitle = '';
+  @Input() bodyText = '';
   @Input() icon = 'description';
 
   constructor() { }
