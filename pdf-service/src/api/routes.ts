1f70852e6daa1ffaebdb89cd666269f981d93b4a
Alejandro Sanchez <emailforasr@gmail.com>
2019-06-03 09:58:47 -0700

    Refactor microservice code

--- pdf-service/src/api/routes.ts
+++ pdf-service/src/api/routes.ts
@@ -1,23 +1,46 @@
 import { Request, Response, Express } from 'express';
-import printPDF from './pdf';
+import { Renderer, printPDF } from '../lib/pdf';
+import { Stopwatch, sysdebug } from '../lib/utils';
 
-export default function applyRoutes(app: Express) {
+export const applyRoutes = (app: Express, renderer?: Renderer) => {
 
   /**
    * POST /pdf
    * PDF rendering.
    */
   app.post('/pdf', async function (req: Request, res: Response) {
-    const { filename = 'unnamed.pdf' } = req.query;
+    const timer = new Stopwatch();
     const html = req.body;
-    console.log('Request to print ' + filename);
-    const pdf = await printPDF(html);
-    console.log(`Sending back PDF of length ${pdf.length}`);
+    let pdf: Buffer;
+    let { filename = 'unnamed.pdf', v = 2 } = req.query;
+    sysdebug(`[/pdf] Request to print ${filename}`);
+
+    // measure execution time
+    timer.start();
+
+    if (!renderer) {
+      sysdebug(`Renderer not found, using legacy PDF generation`);
+      v = 1;
+    }
+
+    if (Number(v) == 1) {
+      sysdebug(`Using legacy PDF mode`);
+      pdf = await printPDF(html);  // legacy
+    } else {
+      pdf = await renderer.pdf(html);
+    }
+
+    sysdebug(`Sending back PDF of length ${pdf.length}`);
+
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Length', pdf.length);
-    res.setHeader('Content-Disposition', `inline;filename=${filename}`);
+    res.setHeader('Content-Disposition', `inline; filename=${filename}`);
     res.send(pdf);
-    console.log(`Done`)
+
+    timer.stop();
+
+    sysdebug(timer.words);
+    sysdebug(`[/pdf] Done`);
   });
 
   /**
@@ -27,4 +50,4 @@ export default function applyRoutes(app: Express) {
   app.get('/health', async function (req: Request, res: Response) {
     res.send(`PDF microservice lives! - ${new Date().toISOString()}`);
   });
-}
+};
