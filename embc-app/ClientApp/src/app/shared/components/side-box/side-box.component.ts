8c5b8e533484d996f5bf73980e1a2fd830ca5ba3
Curtis <curtis.laycraft@zoho.com>
2019-06-03 13:11:01 -0700

    side box styling

--- embc-app/ClientApp/src/app/shared/components/side-box/side-box.component.ts
+++ embc-app/ClientApp/src/app/shared/components/side-box/side-box.component.ts
@@ -8,25 +8,20 @@ import { Component, Input } from '@angular/core';
     <div class="card text-white bg-primary">
       <div class="card-header">
       <app-font-awesome-icon icon="info-circle" size="lg"></app-font-awesome-icon>
-      <span class="h5 ml-2 text-wrap">Useful Information</span>
+      <span class="h5 ml-2 text-wrap">{{title}}</span>
       </div>
       <div class="card-body">
-        <!--<span class="h2 card-title">{{secondaryTitle}}</span>-->
-        <p class="card-text">
-          Evacuees listed here that do not have a task number are self-registered and have not yet received Emergency Support Services.
-        </p>
-        <p class="card-text">
-          Use the “Action” column to “view” evacuee registrations as well as “finalize” evacuee self-registrations.
-        </p>
+        <span class="card-text">
+          <ng-content></ng-content>
+        </span>
       </div>
     </div>
   `,
 })
 export class SideBoxComponent {
 
-  @Input() title = '';
+  @Input() title = 'Useful Information';
   @Input() secondaryTitle = '';
-  @Input() bodyText = '';
   @Input() icon = 'description';
 
   constructor() { }
