dc8e19158eb44add678ff5908a23cface0bb060b
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-06-05 11:04:16 -0700

    Fixed docker file and backup
    Ensure endings are LF
    
    Added path to sql tools
    
    Revised dockerfile to use Microsoft's
    
    Added entry poing for openshift deployment
    
    Added missing uid_entrypoint
    
    Fixed dockerfile
    
    Fixed dockerfile
    
    Test sql server base container
    
    Restore the uid_entrypoint
    
    Reversed the Image to Unbuntu using Sql Server Tools
    
    banging head on table
    
    banging head on table
    
    banging head on table
    
    Uncomment the sql statement
    
    Fix the SQL Statement
    
    Add backup env variable
    
    Added the backup clean up and directory management
    
    Fix else statement
    
    Fix sql statement and add expiry env variable
    
    Fix sql statement and add expiry env variable
    
    Fixing backup statement
    
    Add vim for debugging
    
    add -y to vim install
    
    change if statememnt
    
    reverse if statement

--- db-backup/backup.sh
+++ db-backup/backup.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-# Postgresql automated backup script
+# mssql automated backup script
 # See README.md for documentation on this script
 
 while true; do
 
-#FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
-#DBFILE=$FINAL_BACKUP_DIR"$MSSQL_DATABASE`date +\%Y-\%m-\%d-%H-%M`"
-#echo "Making backup directory in $FINAL_BACKUP_DIR"
+FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
+DBFILE=$FINAL_BACKUP_DIR"$MSSQL_DATABASE`date +\%Y-\%m-\%d-%H-%M`.bak"
+echo "Making backup directory in $FINAL_BACKUP_DIR"
  
-#if ! mkdir -p $FINAL_BACKUP_DIR; then
-#	echo "Cannot create backup directory in $FINAL_BACKUP_DIR." 1>&2
-#	exit 1;
-#fi;
+if ! mkdir -p $FINAL_BACKUP_DIR; then
+	echo "Cannot create backup directory in $FINAL_BACKUP_DIR." 1>&2
+	exit 1;
+fi;
 
 DB_ADMIN_PASSWORD=$DB_ADMIN_PASSWORD
 export DB_ADMIN_PASSWORD
 
-sqlcmd -s $MSSQL_DATABASE -U sa -P $DB_ADMIN_PASSWORD -d master -q "BACKUP DATABASE ess-develop TO DISK='/var/opt/mssql/data/var/opt/mssql/data/ess-develop-zip-test.bak'"
+echo "Backing up $MSSQL_DATABASE on $DATABASE_SERVICE_NAME"
+sqlcmd -S $DATABASE_SERVICE_NAME -U sa -P $DB_ADMIN_PASSWORD -d master -Q "BACKUP DATABASE $MSSQL_DATABASE TO DISK='$DBFILE'"
 
-#if ! /opt/rh/rh-postgresql94/root/usr/bin/pg_dump -Fp -h "$DATABASE_SERVICE_NAME" -U "sa" "$MSSQL_DATABASE" | gzip > $DBFILE.sql.gz.in_progress; then
-#	echo "[!!ERROR!!] Failed to backup database $MSSQL_DATABASE" 
-#else
-#	mv $DBFILE.sql.gz.in_progress $DBFILE.sql.gz
-#	echo "Database backup written to $DBFILE.sql.gz"
-	
-	# first cull backups older than 10 days.  (SB-331)
-#	find $BACKUP_DIR* -type d -ctime +31 | xargs rm -rf
-#fi;
+if test -f "$DBFILE" ; then
+	echo "Database backup written to $DBFILE"
+
+	echo "Will now gzip $DBFILE"
+	gzip $DBFILE
+
+	echo "Database backup written compressed to $DBFILE.gz"
+
+	# first cull backups older. 
+	echo "culling backups older than ${BACKUP_EXPIRY_DAYS}"
+	find $BACKUP_DIR* -type d -ctime +${BACKUP_EXPIRY_DAYS} | xargs rm -rf
+else
+	echo "[!!ERROR!!] Failed to backup database $MSSQL_DATABASE"
+fi;
 
 # 24 hrs
 sleep 1d
