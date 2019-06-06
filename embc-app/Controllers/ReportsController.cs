7df45ae2524c23c5dcabc4ad3ae2c23483a2ab63
Yossi T <48697347+ytqsl@users.noreply.github.com>
2019-06-06 13:14:24 -0700

    EMBCESS-843: backend report controller and query for registration view audit trail (#9)
    * Added audit query service and controller
    
    * Refactored to use mediatR for req/res pattern

---
+++ embc-app/Controllers/ReportsController.cs
@@ -0,0 +1,32 @@
+using Gov.Jag.Embc.Public.Services.Registrations;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace embc_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ReportsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("registration/audit/{id}")]
+        public async Task<IActionResult> GetRegistrationAudit(string id)
+        {
+            if (!long.TryParse(id, out var essFileNumber)) return BadRequest($"'{id}' not a valid ess file number");
+            var registration = await mediator.Send(new RegistrationSummaryQueryRequest(id));
+            if (registration == null) return NotFound();
+            var auditTrail = await mediator.Send(new RegistrationAuditQueryRequest(essFileNumber));
+
+            return Ok(auditTrail);
+        }
+    }
+}
