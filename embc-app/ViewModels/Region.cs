46c97f63a3e7e97663366e4d547c005f5514c374
Yossi Tamari <yossi.tamari@quartech.com>
2019-06-07 11:16:02 -0700

    removed id property from region

--- embc-app/ViewModels/Region.cs
+++ embc-app/ViewModels/Region.cs
@@ -1,9 +1,7 @@
 namespace Gov.Jag.Embc.Public.ViewModels
 {
-
     public class Region
     {
-        public string Id { get; set; }
         public bool? Active { get; set; }
         public string Name { get; set; }
     }
