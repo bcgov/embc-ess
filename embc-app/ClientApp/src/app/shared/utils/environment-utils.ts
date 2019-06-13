6822436c316b59e0a6d43c70a706f76cf0249fab
Yossi Tamari <yossi.tamari@quartech.com>
2019-06-13 13:54:55 -0700

    removed eval

--- embc-app/ClientApp/src/app/shared/utils/environment-utils.ts
+++ embc-app/ClientApp/src/app/shared/utils/environment-utils.ts
@@ -2,9 +2,5 @@
  * detect IE
  */
 export function detectIE10orLower(): boolean {
-  const jscriptVersion = new Function('/*@cc_on return @_jscript_version; @*/')();
-  if (jscriptVersion !== undefined) {
-    return true;
-  }
   return false;
 }
