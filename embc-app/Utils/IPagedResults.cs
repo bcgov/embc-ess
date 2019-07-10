7e81500cdeecd4e1b5ca216d76e1a325f3564b2f
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-07-09 19:57:57 -0700

    Change the query for Evacuees

---
+++ embc-app/Utils/IPagedResults.cs
@@ -0,0 +1,14 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Gov.Jag.Embc.Public.Utils
+{
+    public interface IPagedResults<T>
+    {
+        [JsonProperty(PropertyName = "metadata")]
+        PaginationMetadata Pagination { get; }
+
+        [JsonProperty(PropertyName = "data")]
+        IEnumerable<T> Items { get; }
+    }
+}
