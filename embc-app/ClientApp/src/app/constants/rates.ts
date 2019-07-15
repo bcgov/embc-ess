028aebb2601d29014fa1772a6c1db7c50e70a923
Curtis <curtis.laycraft@zoho.com>
2019-07-15 14:33:14 -0700

    old prices

--- embc-app/ClientApp/src/app/constants/rates.ts
+++ embc-app/ClientApp/src/app/constants/rates.ts
@@ -1,7 +1,12 @@
-// const in dollars for restaurant meals
-export const RESTAURANT_BREAKFAST = 12.25;
-export const RESTAURANT_LUNCH = 14.25;
-export const RESTAURANT_DINNER = 24.50;
+// const in dollars for restaurant meals (SOON TO BE DEPRECATED)
+export const RESTAURANT_BREAKFAST = 10;
+export const RESTAURANT_LUNCH = 13;
+export const RESTAURANT_DINNER = 22;
+
+// NEW RATES TO REPLACE THE ONES ABOVE 2019-06-25
+// export const RESTAURANT_BREAKFAST = 12.25;
+// export const RESTAURANT_LUNCH = 14.25;
+// export const RESTAURANT_DINNER = 24.50;
 
 // const in dollars for groceries
 export const GROCERIES = 22.50;
