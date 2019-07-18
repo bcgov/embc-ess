3a6274b6fbf66a6ecb32d9e02c64981c71789672
Curtis <curtis.laycraft@zoho.com>
2019-07-18 14:53:37 -0700

    order change

--- embc-app/DataInterfaces/DataInterface.Evacuee.cs
+++ embc-app/DataInterfaces/DataInterface.Evacuee.cs
@@ -12,11 +12,25 @@ namespace Gov.Jag.Embc.Public.DataInterfaces
     {
         public async Task<IPagedResults<EvacueeListItem>> GetEvacueesAsync(EvacueeSearchQueryParameters searchQuery)
         {
+            if (searchQuery.HasSortBy()) {
+                // Sort by whatever parameter was included with the query
+                searchQuery.SortBy = MapSortToFields(searchQuery.SortBy);
+            }
+            else
+            {
+                // default search is always sort descending by ess file number
+                searchQuery.SortBy = "-essFileNumber";
+            };
+
             var query = db.ViewEvacuees
-                .Where(e => e.Active == searchQuery.Active);
+                // Inactive evacuees are soft deleted. We do not return them or give the user the option yet.
+                .Where(e => e.Active == searchQuery.Active)
+                // we sort the larger collection first before letting the subset (paginated ones be sorted)
+                .Sort(MapSortToFields(searchQuery.SortBy));
 
             if (searchQuery.HasQuery())
             {
+                // Simple search. When a search query is provided search should match multiple things from the record. Query can match multiple things.
                 query = query.Where(e =>
                     EF.Functions.Like(e.LastName, $"%{searchQuery.Query}%") ||
                     e.IncidentTaskNumber == searchQuery.Query ||
@@ -26,24 +40,40 @@ namespace Gov.Jag.Embc.Public.DataInterfaces
             }
             else
             {
+                // Advanced search. There is a null query. However the client has supplied other specific parameters to search by.
+                // if a search parameter is not null, then add a "where" clause to the query matching the supplied UTF-16 query string
                 if (!string.IsNullOrWhiteSpace(searchQuery.LastName)) query = query.Where(e => EF.Functions.Like(e.LastName, $"%{searchQuery.LastName}%"));
                 if (!string.IsNullOrWhiteSpace(searchQuery.FirstName)) query = query.Where(e => EF.Functions.Like(e.FirstName, $"%{searchQuery.FirstName}%"));
                 if (!string.IsNullOrWhiteSpace(searchQuery.IncidentTaskNumber)) query = query.Where(e => e.IncidentTaskNumber == searchQuery.IncidentTaskNumber);
                 if (!string.IsNullOrWhiteSpace(searchQuery.EssFileNumber)) query = query.Where(e => e.RegistrationId == searchQuery.EssFileNumber);
                 if (!string.IsNullOrWhiteSpace(searchQuery.EvacuatedFrom)) query = query.Where(e => e.EvacuatedFrom == searchQuery.EvacuatedFrom);
                 if (!string.IsNullOrWhiteSpace(searchQuery.EvacuatedTo)) query = query.Where(e => e.EvacuatedTo == searchQuery.EvacuatedTo);
-                if (searchQuery.HasReferrals.HasValue && searchQuery.HasReferrals.Value) query = query.Where(e => e.HasReferrals);
-                if (searchQuery.HasReferrals.HasValue && !searchQuery.HasReferrals.Value) query = query.Where(e => !e.HasReferrals);
+
+                // if has referrals has a value do some things. Else is omit the where clause so it is omitted
+                if (searchQuery.HasReferrals.HasValue)
+                {
+                    // (Why can searchQuery be valueless in the object? It should take memory space whether we intantiate it or not.)
+                    if (searchQuery.HasReferrals.Value) {
+                        // set the "where" clause for only evacuees with referrals
+                        query = query.Where(e => e.HasReferrals);
+                    }
+                    else
+                    {
+                        // set the "where" clause for only evacuees without referrals
+                        query = query.Where(e => !e.HasReferrals);
+                    }
+                }
+                // If the 
                 if (searchQuery.RegistrationCompleted.HasValue) query = query.Where(e => e.IsFinalized == searchQuery.RegistrationCompleted.Value);
             }
 
-            if (!searchQuery.HasSortBy()) searchQuery.SortBy = "-essFileNumber";
-
+            // build the paginated query
             var pagedQuery = new PaginatedQuery<Models.Db.ViewEvacuee>(query, searchQuery.Offset, searchQuery.Limit);
-            query = pagedQuery.Query;
 
-            var results = await query.Sort(MapSortToFields(searchQuery.SortBy)).ToArrayAsync();
+            // get results back from 
+            var results = await pagedQuery.Query.Sort(MapSortToFields(searchQuery.SortBy)).ToArrayAsync();
 
+            // map the evacueeList
             return new PaginatedList<EvacueeListItem>(results.Select(mapper.Map<EvacueeListItem>), pagedQuery.Pagination);
         }
 
