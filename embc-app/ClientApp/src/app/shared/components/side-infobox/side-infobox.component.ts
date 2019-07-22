0607bddd75a8c57cdf27a7a7f1bf92f4615561a3
Curtis <curtis.laycraft@zoho.com>
2019-07-22 13:13:05 -0700

    externalize the sidebox template

--- embc-app/ClientApp/src/app/shared/components/side-infobox/side-infobox.component.ts
+++ embc-app/ClientApp/src/app/shared/components/side-infobox/side-infobox.component.ts
@@ -3,20 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-side-infobox',
   styleUrls: ['./side-infobox.component.scss'],
-  template: `
-
-    <div class="card sidebox-card-info text-white">
-      <div class="card-header text-light">
-      <app-font-awesome-icon icon="info-circle" size="lg"></app-font-awesome-icon>
-      <span class="h5 ml-2 text-wrap">{{title}}</span>
-      </div>
-      <div class="card-body">
-        <span class="card-text text-light">
-          <ng-content></ng-content>
-        </span>
-      </div>
-    </div>
-  `,
+  templateUrl: './side-infobox.component.html',
 })
 export class SideInfoboxComponent {
 
