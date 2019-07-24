6a0d5b48ea02d9959838a1d23badeb802459dc91
Curtis <curtis.laycraft@zoho.com>
2019-07-24 16:10:35 -0700

    change lodging rates

--- embc-app/ClientApp/src/app/shared/modals/lodging-rates/lodging-rates.component.ts
+++ embc-app/ClientApp/src/app/shared/modals/lodging-rates/lodging-rates.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { BILLETING_NIGHTLY, BILLETING_ADDED_ADULT, BILLETING_ADDED_CHILD } from 'src/app/constants/rates';
 @Component({
   templateUrl: './lodging-rates.component.html',
   styleUrls: ['./lodging-rates.component.scss']
 })
 
 export class LodgingRatesComponent {
-
+  nightly = BILLETING_NIGHTLY;
+  adult = BILLETING_ADDED_ADULT;
+  child = BILLETING_ADDED_CHILD;
   constructor(
     public activeModal: NgbActiveModal
   ) { }
