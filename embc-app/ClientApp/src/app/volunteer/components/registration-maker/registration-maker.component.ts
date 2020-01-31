d0639a99388b203e9a9809b93d5ed4fa3d46247a
Wes R <Wes.Reid@Quartech.com>
2020-01-31 09:18:12 -0800

    Remove validation for relationship to evacuee (#873)

--- embc-app/ClientApp/src/app/volunteer/components/registration-maker/registration-maker.component.ts
+++ embc-app/ClientApp/src/app/volunteer/components/registration-maker/registration-maker.component.ts
@@ -294,7 +294,6 @@ export class RegistrationMakerComponent implements OnInit, AfterViewInit {
         initials: fmbr.initials,
         gender: fmbr.gender,
         dob: [fmbr.dob, [Validators.required, CustomValidators.date('YYYY-MM-DD'), CustomValidators.maxDate(moment())]],
-        relationshipToEvacuee: [fmbr.relationshipToEvacuee, Validators.required],
       });
     } else {
       // make a new family member blank and return it.
@@ -305,7 +304,6 @@ export class RegistrationMakerComponent implements OnInit, AfterViewInit {
         initials: '',
         gender: null,
         dob: [null, [Validators.required, CustomValidators.date('YYYY-MM-DD'), CustomValidators.maxDate(moment())]],
-        relationshipToEvacuee: [null, Validators.required],
       });
     }
   }
