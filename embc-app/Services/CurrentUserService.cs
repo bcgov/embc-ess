a15fc3e2471f2b2fa245f871c0614bd66c9f7a05
Wes Reid <Wes.Reid@Quartech.com>
2020-02-10 16:10:05 -0800

    Add CurrentUserService to give handle on current user outside of controller

--- embc-app/Services/CurrentUserService.cs
+++ embc-app/Services/CurrentUserService.cs
@@ -1,43 +1,32 @@
 using Gov.Jag.Embc.Public.Authentication;
+using Gov.Jag.Embc.Public.Utils;
 using Gov.Jag.Embc.Public.ViewModels;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace Gov.Jag.Embc.Public.Services
 {
-    public class CurrentUserService
+    public class CurrentUserService : ICurrentUser
     {
-        private IHttpContextAccessor _context;
-        private ClaimsPrincipal _principal;
-        private User _user;
+        private readonly ClaimsPrincipal _principal;
+        public User CurrentUser { get; }
+
         public CurrentUserService(IHttpContextAccessor context)
         {
-            _context   = context;
-            _principal = _context.HttpContext.User;
-        }
-
-        public User GetCurrentUser()
-        {
-            if (_user == null)
+            _principal = context.HttpContext.User;
+            CurrentUser = new User()
             {
-                _user = new User()
-                {
-                    appRoles  = _principal.FindAll(ClaimTypes.Role).Select(c => c.Value).ToArray(),
-                    name      = _principal.FindFirstValue(SiteMinderClaimTypes.NAME),
-                    firstname = _principal.FindFirstValue(ClaimTypes.GivenName),
-                    lastname  = _principal.FindFirstValue(ClaimTypes.Surname),
-                    UserType  = _principal.FindFirstValue(SiteMinderClaimTypes.USER_TYPE),
-                    contactid = _principal.FindFirstValue(EssClaimTypes.USER_ID),
-                    id        = _principal.FindFirstValue(ClaimTypes.Upn),
-                    accountid = _principal.FindFirstValue(EssClaimTypes.ORG_ID)
-                };
-            }
-
-            return _user;
+                appRoles  = _principal.FindAll(ClaimTypes.Role).Select(c => c.Value).ToArray(),
+                name      = _principal.FindFirstValue(SiteMinderClaimTypes.NAME),
+                firstname = _principal.FindFirstValue(ClaimTypes.GivenName),
+                lastname  = _principal.FindFirstValue(ClaimTypes.Surname),
+                UserType  = _principal.FindFirstValue(SiteMinderClaimTypes.USER_TYPE),
+                contactid = _principal.FindFirstValue(EssClaimTypes.USER_ID),
+                id        = _principal.FindFirstValue(ClaimTypes.Upn),
+                accountid = _principal.FindFirstValue(EssClaimTypes.ORG_ID)
+            };
         }
+
     }
 }
