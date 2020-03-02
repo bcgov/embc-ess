a1e125d762c92053140bacf7cfd0a7e0bd5d8764
Yossi T <48697347+ytqsl@users.noreply.github.com>
2020-03-02 11:54:42 -0800

    Fixed broken build
    https://justice.gov.bc.ca/jira/browse/EMBCESSMOD-72

--- embc-app/Startup.cs
+++ embc-app/Startup.cs
@@ -20,6 +20,7 @@ using Microsoft.Extensions.HealthChecks;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
