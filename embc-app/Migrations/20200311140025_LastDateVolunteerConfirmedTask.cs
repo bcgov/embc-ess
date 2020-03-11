b81fa2deeafe96c7b1ea5cd6457750abc7ee1bd4
Moffat Sehudi <msehudi@gmail.com>
2020-03-11 07:47:36 -0700

    Persist task number on browser refresh

---
+++ embc-app/Migrations/20200311140025_LastDateVolunteerConfirmedTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gov.Jag.Embc.Public.Migrations
+{
+    public partial class LastDateVolunteerConfirmedTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastDateVolunteerConfirmedTask",
+                table: "VolunteerTasks",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastDateVolunteerConfirmedTask",
+                table: "VolunteerTasks");
+        }
+    }
+}
