2710539ffef5039a88493265ac5368ef01ff4fde
Wes Reid <Wes.Reid@Quartech.com>
2020-03-13 16:28:37 -0700

    Hide info box on user detail summary

--- embc-app/ClientApp/src/app/provincial-admin/pages/admin-volunteer-maker-page/admin-volunteer-maker-page/admin-volunteer-maker-page.component.ts
+++ embc-app/ClientApp/src/app/provincial-admin/pages/admin-volunteer-maker-page/admin-volunteer-maker-page/admin-volunteer-maker-page.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-volunteer-maker-page.component.scss']
 })
 export class AdminVolunteerMakerPageComponent implements OnInit {
-
+  onSummaryPage: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSummaryChange(onSummary: boolean) {
+    this.onSummaryPage = onSummary;
   }
 
 }
