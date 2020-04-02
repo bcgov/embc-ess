a4b7035d6f12693b3267afd34d03fe715f40e93f
Wes Reid <Wes.Reid@Quartech.com>
2020-04-01 17:51:18 -0700

    Add PDF links

--- embc-app/ClientApp/src/app/volunteer/pages/useful-information-page/useful-information-page.component.ts
+++ embc-app/ClientApp/src/app/volunteer/pages/useful-information-page/useful-information-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { User } from 'src/app/core/models';
 
 @Component({
   templateUrl: './useful-information-page.component.html',
@@ -14,6 +13,16 @@ export class UsefulInformationPageComponent implements OnInit {
     this.isSuperUser = this.authService.isSuperUserOrAdmin();
   }
 
-  
+  openRefGuide() {
+    const url = "https://ess.pathfinder.gov.bc.ca/era/documents/ERA-Quick-Reference-Guide-Getting-Started.pdf";
+    window.open(url, "_blank");
+  }
+
+  openTrainingGuide() {
+    const url = this.isSuperUser
+                ? "https://ess.pathfinder.gov.bc.ca/era/documents/ERA-User-Guide-ERA-Superusers.pdf"
+                : "https://ess.pathfinder.gov.bc.ca/era/documents/ERA-User-Guide-ERA-Users.pdf";
+    window.open(url, "_blank");
+  }
 
 }
